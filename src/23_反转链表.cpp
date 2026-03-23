3360d022aeb7976a427ce9ef439b53174af88394
monsoon9 <1437607425@qq.com>
2026-03-23 14:37:59 +0800

    使用pre记录前一个链表值

---
+++ src/23_反转链表.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode() : val(0),next(nullptr) {}
+    ListNode(int x) : val(x),next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x),next(next) {} 
+};
+
+
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+        while(cur!=nullptr){
+            ListNode* nextTemp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = nextTemp;
+        }
+        return pre;
+    }
+};
